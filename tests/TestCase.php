2f78141d76e2c08e340bb2722a8e47b63dd60e5a
USAMI Kenta <tadsan@zonu.me>
2019-12-04 22:13:31 +0900

    composer require --dev phpunit/phpunit:^7.5

---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bag2\OAuth\PKCE;
+
+/**
+ * Abstract class of PHPUnit TestCase
+ *
+ * @copyright 2019 Baguette HQ
+ * @license Apache-2.0
+ * @author USAMI Kenta <tadsan@zonu.me>
+ */
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Bootstrap file for testing
+ *
+ * @copyright 2019 Baguette HQ
+ * @license Apache-2.0
+ * @author USAMI Kenta <tadsan@zonu.me>
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
